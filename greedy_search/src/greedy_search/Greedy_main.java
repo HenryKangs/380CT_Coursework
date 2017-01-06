63a12d1f835ee668d19ca3930c167ada71cef16d
HenryKangs <gear7755@gmail.com>
2017-01-06 16:56:26 +0300

    Add files via upload

---
+++ greedy_search/src/greedy_search/Greedy_main.java
@@ -0,0 +1,56 @@
+package greedy_search;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Random;
+
+public class Greedy_main {
+
+    public static void main(String[] args) {
+        int bit = 20;
+        int total = 0;
+        for (int n = 20; n < 140; n = n + 4) {
+            total = 0;
+            for (int i = 0; i < 100; i++) {
+                total += begin(n, bit);
+            }
+            System.out.println(total/100);
+        }
+    }
+
+    public static int begin(int n, int bit) {
+        int max_n_bit_number = (int) Math.pow(2, bit - 1);
+        Integer[] S = new Integer[n];
+        Random rand = new Random();
+        int temp;
+        for (int i = 0; i < n; i++) {
+            temp = rand.nextInt(max_n_bit_number) + 0;
+            S[i] = temp;
+        }
+        int t = 10000;
+        Arrays.sort(S, Collections.reverseOrder());
+        return SubsetSum_Greedy(S, n, t);
+    }
+
+    public static int SubsetSum_Greedy(Integer S[], int n, int t) {
+        int total = 0;
+        int i = 0;
+//        List subset = new ArrayList<>();
+
+        while (i <= n - 1) {
+            if (total + S[i] <= t) {
+                total = total + S[i];
+//                subset.add(array[i]);
+            }
+            if (total == t) {
+                break;
+            }
+            i++;
+        }
+
+//        for (int a = 0; a < subset.size(); a++) {
+//            System.out.print(subset.get(a).toString() + " ");
+//        }
+        return t - total;
+    }
+}
